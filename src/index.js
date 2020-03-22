613c874334c841f394b82931b245cb91ab9f043c
Henderson Pinto <hendersoncode@gmail.com>
2020-03-22 21:54:07 +0100

    Added contact form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Webpack starts here the bundling and the dependencies management. And also in vendor.js
 // Here I should include all the js files that I am going to use on the websites
-import {componentFunction} from "./app/component"
-import "./stylesheets/index.scss"       // This is how I include the css to the webpack so it then can include it to the HTML
+import { scrollTo } from './app/scrollTo';
+import './stylesheets/index.scss'; // This is how I include the css to the webpack so it then can include it to the HTML
+import { changeContent } from './app/changeContent';
 
-
-componentFunction();
-console.log('from bundle');
+scrollTo();
+changeContent();
--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,37 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: {app: "./src/index.js",            // Where the bundling should start bundling
-          vendor: "./src/vendor.js"},       // Where the bundling should start bundling
-  module: {
-    rules: [
-      {
-        test: /\.html$/,                     // When encounters this regex (HTML files) then use
-        use: ["html-loader"]                 // What to use for HTML files. This loader will handle the img or assets files by changing src to require
-      },
-      {
-        test: /\.(svg|png|jpg|jpeg|gif)$/,   // What type of assets include
-        use: {
-          loader: "file-loader",             // When encounter one of those files, this loader is needed for html-loader to work. This handles all the require statements
-          options:{
-            name:"[name].[hash].[ext]",      // Each asset will have a hash in their name so it loads only if we change the file
-            outputPath: "images"
-          }
-        }
-      }
-    ]
-  }
-
+	entry: {
+		app: './src/index.js', // Where the bundling should start bundling
+		vendor: './src/vendor.js'
+	}, // Where the bundling should start bundling
+	module: {
+		rules: [
+			{
+				test: /\.html$/, // When encounters this regex (HTML files) then use
+				use: ['html-loader'] // What to use for HTML files. This loader will handle the img or assets files by changing src to require
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif)$/, // What type of assets include
+				use: {
+					loader: 'file-loader', // When encounter one of those files, this loader is needed for html-loader to work. This handles all the require statements
+					options: {
+						name: '[name].[hash].[ext]', // Each asset will have a hash in their name so it loads only if we change the file
+						outputPath: 'images'
+					}
+				}
+			},
+			{
+				test: /\.svg$/, // What type of assets include
+				use: {
+					loader: 'file-loader', // When encounter one of those files, this loader is needed for html-loader to work. This handles all the require statements
+					options: {
+						name: '[name].[ext]', // Each asset will have a hash in their name so it loads only if we change the file
+						outputPath: 'images'
+					}
+				}
+			}
+		]
+	}
 };
